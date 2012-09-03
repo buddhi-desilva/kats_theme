e61350129d5b23fb27e54710f01b83b66b01f3b7
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-09-03 12:33:46 +0800

    UI Tweaks, Routes & Login/Logout links/styles

--- app/helpers/application_helper_decorator.rb
+++ app/helpers/application_helper_decorator.rb
@@ -4,7 +4,7 @@ ApplicationHelper.class_eval do
   def link_to_spree_cart
     unless !current_spree_page?(spree.cart_path) and (current_order.nil? or current_order.line_items.empty?)
       text = "#{t('spree.cart')} <span class='circled-numbers-cart'> #{current_order.item_count} </span>".html_safe
-      return "<span id='nav-cart-block'>#{link_to text, spree.cart_path}</span>".html_safe
+      return "<span id='nav-cart-block'>#{link_to text, spree.cart_path}</span>#{user_sign_in_link}".html_safe
     end    
   end
 
@@ -16,6 +16,18 @@ ApplicationHelper.class_eval do
       return path == spree.url_for(url)
     end
     return false
+  end
+
+  def user_sign_in_link
+    login_text = "<span class='user-box'>"
+    if spree_current_user
+      login_text << "<span class='account'>#{link_to t(:my_account), spree.account_path}</span>"
+      login_text << "<span class='logout'>#{link_to t(:logout), spree.destroy_user_session_path}</span>"
+    else
+      login_text << "<span class='login'>#{link_to t(:login), spree.login_path}</span>"
+    end
+    login_text << "</span>"
+    return login_text.html_safe
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,91 @@
+# Refinery::Core::Engine.routes.draw do
+#   namespace :blog, :path => 'of-beautiful-things' do
+#     namespace :admin, :path => 'admin/refinery/blog' do
+#       scope :path => 'admin/refinery/blog' do
+
+#         resources :posts do
+#           collection do
+#             get   :home_posts
+#             post  :home_posts_update_positions
+#             post  :home_posts_remove
+#           end
+#         end
+
+#       end
+#     end
+#   end
+# end
+
+
 Refinery::Core::Engine.routes.draw do
+  match "/blog", :to => redirect("/of-beautiful-things")
+  match "/refinery", :to => redirect("/admin/cms")
+  match "/refinery/blog", :to => redirect("/admin/cms/blog")
+
+  # match "/refinery/images", :to => redirect("/admin/cms/images")
+  # match "/refinery/pages", :to => redirect("/admin/cms/pages")
+  # match "/refinery/settings", :to => redirect("/admin/cms/settings")
+
+
+
+
+  namespace :blog, :path => 'of-beautiful-things' do
+    root :to => "posts#index"
+    resources :posts, :only => [:show]
+
+    match 'feed.rss', :to => 'posts#index', :as => 'rss_feed', :defaults => {:format => "rss"}
+    match 'categories/:id', :to => 'categories#show', :as => 'category'
+    match ':id/comments', :to => 'posts#comment', :as => 'comments'
+    get 'archive/:year(/:month)', :to => 'posts#archive', :as => 'archive_posts'
+    get 'tagged/:tag_id(/:tag_name)' => 'posts#tagged', :as => 'tagged_posts'
+  end
+
+  namespace :admin, :path => 'admin/cms' do
+    root :to => 'dashboard#index'
+    resources :dialogs, :only => [:index, :show]
+  end
+
+
+
   namespace :blog, :path => '' do
-    namespace :admin, :path => 'refinery' do
+    namespace :admin, :path => 'admin/cms' do
       scope :path => 'blog' do
+        root :to => "posts#index"
 
         resources :posts do
           collection do
+            get :uncategorized
+            get :tags
             get   :home_posts
             post  :home_posts_update_positions
             post  :home_posts_remove
+
           end
         end
 
+        resources :categories
+
+        resources :comments do
+          collection do
+            get :approved
+            get :rejected
+          end
+          member do
+            post :approve
+            post :reject
+          end
+        end
+
+        resources :settings do
+          collection do
+            get :notification_recipients
+            post :notification_recipients
+
+            get :moderation
+            get :comments
+            get :teasers
+          end
+        end
       end
     end
   end
