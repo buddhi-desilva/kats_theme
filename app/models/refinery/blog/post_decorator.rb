8335e11c8919e069033d8e35886e7d4c8b44f657
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-09-05 00:58:25 +0800

    Fixes + Theme Change requests
    * Issue fix: blog auto adds/increments the home page position. No
    position is set unless it's marked homepage product
    * Misc UI Change requests

--- app/models/refinery/blog/post_decorator.rb
+++ app/models/refinery/blog/post_decorator.rb
@@ -1,12 +1,20 @@
 Refinery::Blog::Post.class_eval do
   acts_as_list :scope => 'show_on_homepage = 1'
   
-  attr_accessible :show_on_homepage
+  attr_accessible :show_on_homepage, :position
   attr_reader :on_homepage
+
+  before_save :position_adjust
 
   def self.on_homepage(limit = 10)
       with_exclusive_scope { self.where(:show_on_homepage => true).order(:position).limit(limit.to_i) }
   end
 
+
+  private
+  ###########################
+  def position_adjust
+    self.position = nil unless self.show_on_homepage ==  true
+  end
 
 end
