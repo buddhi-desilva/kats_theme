77b12d94a6321f4f33adcb0716bcd4d81bab4ec1
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-10-01 17:49:49 +0800

    Removed: unwated file

--- app/overrides/test.rb
+++
@@ -1,6 +0,0 @@
-# Deface::Override.new(
-#   :name           =>  "replace_refinery_application_layout",
-#   :virtual_path   =>  'refinery/layouts/application',
-#   :replace        =>  "footer#footer",
-#   :partial        =>  "refinery/layouts/application"
-#   )
