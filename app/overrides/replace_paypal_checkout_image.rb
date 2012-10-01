4e49e8752aa90109d8013cd82d4be1f80acded28
Buddhi DeSilva <buddhi.desilva@gmail.com>
2012-10-01 23:25:24 +0800

    Change PayPal payment button image

--- app/overrides/replace_paypal_checkout_image.rb
+++ app/overrides/replace_paypal_checkout_image.rb
@@ -2,6 +2,6 @@ Deface::Override.new(:virtual_path => "spree/checkout/_paypal_checkout",
                      :name => "replace_paypal_checkout_image",
                      :replace => "code[erb-loud]:contains('image_submit_tag')",
                      :text => %q{
-                        <%= image_submit_tag "kats_pp_checkout.gif", :alt => t(:check_out_with_paypal) %>
+                        <%= image_submit_tag "kats_pp_checkout.png", :alt => t(:check_out_with_paypal) %>
                       },
                      :disabled => false)
